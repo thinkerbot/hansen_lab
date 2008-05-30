e8218faf5324879172119bf1c50683ab1ac26e57
Simon Chiang <simon.a.chiang@gmail.com>
2008-05-30 17:48:16 -0600

    Fixed indentation.

--- servers/bc.uchsc.edu/lib/user/create.rb
+++ servers/bc.uchsc.edu/lib/user/create.rb
@@ -1,65 +1,60 @@
 module User
 
-# == Description
-# Creates a new group user.
-#
-# created by: Simon Chiang
-# date:  2006-04-21  original .sh script
-# date:  2008-05-30  ported to tap
-# run as: root
-#
-# This administrative script creates a standard user
-# on the server.  Users are created with the following:
-#   -the full name	  (John Doe)	  {fullname}
-#   -the user name	  (doej)		    {username}
-#   -the group    	  (msf) 		    {group}
-#   -the group admin	(msf_admin)	  {group_admin} 
-#
-# The script prompts for all passwords as it runs; it
-# is not possible to enter passwords into the command
-# line.
-#
-# Users are created to be able to access the server 
-# via Samba. Samba is the program that allows Windows 
-# to access drives on Linux.  
-#
-# The new user will belong to group, and be given the 
-# standard Samba share setup with the script
-# modifySambaConfig.pl
-#
-# The new user will be given a home data folder accessible
-# only by them and the group administrator.
-#
-#   /home/user               user, group_admin         rwxrwx---   (mod/access for user and group admin only)
-#   |- group           link
-#   `- mirror/archive  link
-#
+  # == Description
+  # Creates a new group user.
+  #
+  # created by: Simon Chiang
+  # date:  2006-04-21  original .sh script
+  # date:  2008-05-30  ported to tap
+  # run as: root
+  #
+  # This administrative script creates a standard user
+  # on the server.  Users are created with the following:
+  #   -the full name    (John Doe)    {fullname}
+  #   -the user name    (doej)        {username}
+  #   -the group        (msf)         {group}
+  #   -the group admin  (msf_admin)   {group_admin} 
+  #
+  # The script prompts for all passwords as it runs; it
+  # is not possible to enter passwords into the command
+  # line.
+  #
+  # Users are created to be able to access the server 
+  # via Samba. Samba is the program that allows Windows 
+  # to access drives on Linux.  
+  #
+  # The new user will belong to group, and be given the 
+  # standard Samba share setup with the script
+  # modifySambaConfig.pl
+  #
+  # The new user will be given a home data folder accessible
+  # only by them and the group administrator.
+  #
+  #   /home/user               user, group_admin         rwxrwx---   (mod/access for user and group admin only)
+  #    |- group           link
+  #    `- mirror/archive  link
+  #
   class Create < Tap::Task
     include Tap::Support::ShellUtils
-    
-    config :home_dir, "/home"
-    
+
     def process(fullname, username, group, group_admin)
 
-home = app.filepath(home_dir, username)
+      home = File.join("/home", username)
 
-#
-# Add the user
-#
+      # Add the user
+      log "Adding the user #{username}..."
+      sh %Q{useradd -m -s /bin/bash -g #{group} -c "#{fullname}" #{username}}
+      sh %Q{usermod -G users,#{group} #{username}}
 
-log "Adding the user #{username}..."
-sh %Q{useradd -m -s /bin/bash -g #{group} -c "#{fullname}" #{username}}
-sh %Q{usermod -G users,#{group} #{username}}
+      # Modify the home directory of the added user
+      sh %Q{chmod 750 #{home}}
+      sh %Q{chown #{username} #{home}}
+      sh %Q{chgrp #{group_admin} #{home}}
 
-#modify the home directory of the added user
-sh %Q{chmod 750 #{home}}
-sh %Q{chown #{username} #{home}}
-sh %Q{chgrp #{group_admin} #{home}}
-
-puts "Please enter a password for '#{username}'."
-sh %Q{passwd #{username}}
-
+      puts "Please enter a password for '#{username}':"
+      sh %Q{passwd #{username}}
+      
+      nil
     end
-    
   end
 end
--- servers/bc.uchsc.edu/lib/user/samba/remove.rb
+++ servers/bc.uchsc.edu/lib/user/samba/remove.rb
@@ -1,35 +1,34 @@
 module User
   module Samba
-  # == Description
-# Removes the specified user from the samba server.
-#
-# created by: Simon Chiang
-# date:  2006-06-12  original .sh script
-# date:  2006-06-12  annotation and testing
-# date:  2008-05-30  ported to tap
-# run as: root
-#
-# This administrative script removes a standard share 
-# from a samba configuration file.  The samba config
-# file is usually located at: /etc/samba/smb.conf
-#
-# The standard share is:
-# [username]
-#   comment = user home
-#   path = /home/username
-#   valid users = username
-#   public = no
-#   browseable = no
-#   writable = yes
-#   create mask = 0660
-#   directory mask = 0770
-#
+    # == Description
+    # Removes the specified user from the samba server.
+    #
+    # created by: Simon Chiang
+    # date:  2006-06-12  original .sh script
+    # date:  2006-06-12  annotation and testing
+    # date:  2008-05-30  ported to tap
+    # run as: root
+    #
+    # This administrative script removes a standard share 
+    # from a samba configuration file.  The samba config
+    # file is usually located at: /etc/samba/smb.conf
+    #
+    # The standard share is:
+    # [username]
+    #   comment = user home
+    #   path = /home/username
+    #   valid users = username
+    #   public = no
+    #   browseable = no
+    #   writable = yes
+    #   create mask = 0660
+    #   directory mask = 0770
+    #
     class Remove < Tap::Task
-
-    
-    config :smbpasswd_file, '/etc/samba/smbpasswd'
-    config :smb_config_file, '/etc/samba/smb.conf'
-    config :share_template, %Q{
+      
+      config :smbpasswd_file, '/etc/samba/smbpasswd'
+      config :smb_config_file, '/etc/samba/smb.conf'
+      config :share_template, %Q{
 [<%= username %>]
   comment = user home
   path = /home/<%= username %>
@@ -42,30 +41,30 @@ module User
 }
 
       def process(username)
-log "Attempting to remove '#{username}' from the samba server..."
+        log "Attempting to remove '#{username}' from the samba server..."
 
-# try removing the standard share from the config file.
-contents = File.read(smb_config_file)
-unless contents =~ Regexp.new(share)
-  raise "The user could not be removed from samba server.  Missing share:\n#{share}"
-end
+        # try removing the standard share from the config file.
+        contents = File.read(smb_config_file)
+        unless contents =~ Regexp.new(share)
+          raise "The user could not be removed from samba server.  Missing share:\n#{share}"
+        end
 
-prepare(smb_config_file)
-File.open(smb_config_file, "w") do |file|
-  file << contents.gsub(share, "")
-end
+        prepare(smb_config_file)
+        File.open(smb_config_file, "w") do |file|
+          file << contents.gsub(share, "")
+        end
 
-  log "Removing the user from the password file..."
-  # only remove the user from the smbpasswd file if
-  # the config file script was successful
-  sh %Q{smbpasswd -x #{username}}
+        log "Removing the user from the password file..."
+        # only remove the user from the smbpasswd file if
+        # the config file script was successful
+        sh %Q{smbpasswd -x #{username}}
 
-# restart the samba server
-log "Restarting the samba server."
-sh Q%{/etc/init.d/smb restart}
-
+        # restart the samba server
+        log "Restarting the samba server."
+        sh Q%{/etc/init.d/smb restart}
+        
+        nil
       end
-      
     end
   end
 end
